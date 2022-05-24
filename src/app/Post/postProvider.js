22fc5713a037aac88f66499302ee268fb60cadd6
HaeChan Kim <bluesun147@naver.com>
2022-05-24 23:36:28 +0900

    이미지 조회

--- src/app/Post/postProvider.js
+++ src/app/Post/postProvider.js
@@ -15,6 +15,12 @@ exports.retrievePostList = async function(userIdx) {
     const connection = await pool.getConnection(async (conn) => conn);
     const postListResult = await postDao.selectPosts(connection, userIdx);
 
+    for (post of postListResult) { // 반복문 통해 뽑아와서 post 변수에 할당
+        const postIdx = post.postIdx; // 게시물 사진 하나하나 조회
+        const postImgResult = await postDao.selectPostImgs(connection, postIdx);
+        post.imgs = postImgResult; // imgs 속성 넣기
+    }
+
     connection.release();
 
     return postListResult;
